3ba5f287311e57996b07f6c048b90dc0912e4a3a
Gruve_p <gruve-p@users.noreply.github.com>
2015-09-12 13:59:48 +0200

    Update create_user.php

--- test/create_user.php
+++ test/create_user.php
@@ -1,12 +1,12 @@
 <?php
 
-require "dogecoin.php";
+require "groestlcoin.php";
 
 $settings = [
 	//wallet connection data (RPC)
 	"rpc_ip" 		=> "localhost",		
-	"rpc_port"		=> 44555,
-	"rpc_user" 		=> "dogecoinrpc",
+	"rpc_port"		=> 1441,
+	"rpc_user" 		=> "groestlcoinrpc",
 	"rpc_password" 		=> "password",
 	"rpc_protocol"		=> "https",
 
@@ -14,13 +14,13 @@ $settings = [
 	"db_server" 		=> 'localhost',
 	"db_username"		=> "root",
 	"db_password" 		=> "",
-	"db_database" 		=> "DogeFrame"
+	"db_database" 		=> "GroestlFrame"
 ];
 	
 $mysqli = mysqli_connect($settings['db_server'] , $settings['db_username'], $settings['db_password'], $settings['db_database']);
-$dogecoin = new Dogecoin($settings['rpc_user'], $settings['rpc_password'], $settings['rpc_ip'], $settings['rpc_port'], $settings['rpc_protocol'] );
-$address = $dogecoin->getnewaddress('DogeFrame');
-if ($stmt = $mysqli->prepare('INSERT IGNORE INTO usertable (doge_address) VALUES (?)')) {
+$groestlcoin = new Groestlcoin($settings['rpc_user'], $settings['rpc_password'], $settings['rpc_ip'], $settings['rpc_port'], $settings['rpc_protocol'] );
+$address = $groestlcoin->getnewaddress('GroestlFrame');
+if ($stmt = $mysqli->prepare('INSERT IGNORE INTO usertable (groestl_address) VALUES (?)')) {
 	$stmt->bind_param("s", $address);
 	if (!$stmt->execute()) {
 		throw new Exception($mysql->error);
